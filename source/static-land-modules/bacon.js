87a2831311dee002a33bea7fe10c9219ceca7978
Matthias Seemann <296476+semmel@users.noreply.github.com>
2020-03-12 20:33:02 +0100

    placeholder file to interface with bacon.js

---
+++ source/static-land-modules/bacon.js
@@ -0,0 +1,7 @@
+/**
+ * Derive algebras for Bacon.js from @most/core
+ * https://mostcore.readthedocs.io/en/latest/api.html#ap
+ *
+ * Created by Matthias Seemann on 31.12.2019.
+ * Copyright (c) 2019 Visisoft OHG. All rights reserved.
+ */
